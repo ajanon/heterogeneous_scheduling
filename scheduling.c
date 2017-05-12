f87cf6c9e69452063a6a51cc5691771038703c0e
Alexis Janon <98706738+ajanon@users.noreply.github.com>
2017-05-12 03:42:08 +0200

    Updated array sizes in main program
    The array are now correctly sized, based on the changes
    in the model.

--- scheduling.c
+++ scheduling.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 	struct cli_args parsed_args = parse_args(argc, argv);
 	int group_count = parsed_args.group_count;
 	int task_count = parsed_args.task_count;
-	int array_size = 2*group_count+8*task_count+1;
+	int array_size = 2*group_count+10*task_count+1;
 	glp_prob *lp = glp_create_prob();
 	int *ia = calloc(array_size, sizeof(int));
 	int *ja = calloc(array_size, sizeof(int));
