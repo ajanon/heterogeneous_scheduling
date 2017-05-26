749e01e3d99e97e1f5e754d14cd1e33b7706afc9
Alexis Janon <98706738+ajanon@users.noreply.github.com>
2017-05-26 11:04:44 +0200

    Added licence notice in source files
    The licence notice was added as a header for all files.

--- plot.R
+++ plot.R
@@ -1,3 +1,25 @@
+# plot.R
+
+###############################################################################
+# Copyright (C) 2017 Alexis Janon
+# 
+# This code is part of a program computing task lists for
+# heterogeneous scheduling problems.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version. 
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
 d <- read.csv("results_plot.csv", sep=";", header=T)
 d <- d[d$TIME < 800,]
 d$TYPE[d$TYPE==0] <- "new"
